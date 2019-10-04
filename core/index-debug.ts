e44cbfb81f28711ad4daced8c723ffc1d4174127
Sergey Kravchenko <sergey@kazuhm.com>
2019-10-03 22:45:28 -0700

    Copy files

--- core/index-debug.ts
+++ core/index-debug.ts
@@ -1,6 +1,26 @@
-const {execSync} = require('child_process');
-
 // copy node_modules from the module folder to build
-execSync(`if test -e "./modules/${process.env.MODULE}/node_modules"; then cp -r ./modules/${process.env.MODULE}/node_modules/* ./build/modules/${process.env.MODULE}/node_modules ; fi`);
 
+/*
+//========== OS specific copy files approach ================================
+const {execSync} = require('child_process');
+execSync(`if test -e "./modules/${process.env.MODULE}/node_modules"; then cp -r ./modules/${process.env.MODULE}/node_modules/* ./build/modules/${process.env.MODULE}/node_modules ; fi`);
+*/
+
+//========== OS agnostic copy files approach ================================
+const copyfiles = require('copyfiles');
+
+console.info('copying node_modules...');
+copyfiles(
+  [
+    `./modules/${process.env.MODULE}/node_modules/**/*`,
+    `./build/modules/${process.env.MODULE}/node_modules`,
+  ],
+  {
+    all: true,
+    up: 3, // slice out ./modules/<module_name>/node_modules
+  },
+  () => {} // mandatory callback
+);
+
+//========== run app ===========================================================
 require('./index');
--- core/kmf-core.js
+++ core/kmf-core.js
@@ -1,5 +1,5 @@
 // this proxy module is copied to node_modules,
 // helps use the non-relative path to import the core module in all KMF modules
 
-const core = require('../core/Core');
+const core = require('../build/core/Core');
 module.exports = core;

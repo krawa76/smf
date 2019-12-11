feb27f3afd1a3d8a90063c3746d857c80937928f
Sergey Kravchenko <sergey@kazuhm.com>
2019-12-11 13:53:32 -0800

    Sails wording

--- modules/demo-web-sails/Main.ts
+++ modules/demo-web-sails/Main.ts
@@ -1,6 +1,6 @@
 export default class Main {
   async run(core) {
-    core.log('demo-web');
+    core.log('demo-web-sails');
     // require(`${core.config.SAILS_PATH}/app`);
     const start = require(`${core.config.SAILS_PATH}/app-parametrized`);
     start(core);

6765b7903ef4a7f9bc8b991a3a8ccabadde8e245
Sergey Kravchenko <sergey@kazuhm.com>
2019-12-14 14:58:51 -0800

    Comments

--- modules/demo-frontend-react/Main.ts
+++ modules/demo-frontend-react/Main.ts
@@ -1,12 +1,9 @@
+// Main.ts, package.json & package-lock.json are skipped, see the custom Dockerfile in this folder
+
 import core from 'smf-core';
 
 export default class Main {
   async run(coreParam) {
     core.log('demo-frontend-react');
-
-    setInterval(async () => {
-      core.log(`test`);
-    },
-    5000);
   }
 }

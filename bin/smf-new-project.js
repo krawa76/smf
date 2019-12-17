7f1b1e7f4375775c533d58cd3cbab5ccc4ca1204
Sergey Kravchenko <sergey@kazuhm.com>
2019-12-16 18:11:48 -0800

    New project: copy content proto

--- bin/smf-new-project.js
+++ bin/smf-new-project.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const copyfiles = require('copyfiles');
+
 const validators = require('./validators');
 
 function newProject() {
@@ -6,13 +10,69 @@ function newProject() {
     return;
   }
 
-  const name = process.argv[3];
+  const projectName = process.argv[3];
 
-  if (!validators.IsGenericNameValid(name)) {
+  if (!validators.IsGenericNameValid(projectName)) {
     return;
   }
 
-  console.info(`Creating new project: ${name}`);
+  console.info(`Creating new project: ${projectName}`);
+
+  if (fs.existsSync(projectName)) {
+    console.error(`Directory already exists: ${projectName}`);
+    return;
+  }
+
+  fs.mkdirSync(projectName, {recursive: true});
+
+  // get SMF root dir name
+  const scriptFilename = process.argv[1];
+  const realScriptFilename = fs.realpathSync(scriptFilename);
+  const realScriptDir  = path.dirname(realScriptFilename);
+  const segs = realScriptDir.split(path.sep);
+  segs.pop();
+  const smfDir = segs.join(path.sep);
+
+  //========== copy content =====================================================
+  console.info('Copying components...');
+
+  function copyFiles(src, dst = '') {
+    console.info(src);
+
+    copyfiles(
+      [
+        `${smfDir}/${src}`,
+        `./${projectName}${dst}`,
+      ],
+      {
+        all: true,
+        up: smfDir.split(path.sep).length, // slice out upper folders
+      },
+      () => {
+        // console.info(`${src} copied`)
+      }
+    );  
+  }
+
+  copyFiles('.vscode/**/*');
+  copyFiles('core/**/*');
+  copyFiles('modules/demo/**/*');
+
+  copyFiles('.gitignore');
+  copyFiles('Dockerfile');
+  copyFiles('tsconfig.json');
+
+  // todo: copy & adjust package.json
+  // todo: readme.md ?
+  // todo: run npm install
+
+  // create smf-stack.json
+  copyFiles('templates/new-project.smf-stack.json');
+  // fs.renameSync(`./${projectName}/new-project.smf-stack.json`, 'smf-stack.json')
+
+  //========== post-install info ===============================================
+  console.info('Done.');
+  // todo: cd ${projectName}; smf up
 }
 
 module.exports = newProject;

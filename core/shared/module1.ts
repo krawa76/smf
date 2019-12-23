9efc4aa1494fe21007296c68768a8e7d648a8f65
Sergey Kravchenko <sergey@kazuhm.com>
2019-12-22 23:52:02 -0800

    Wording

--- core/shared/module1.ts
+++ core/shared/module1.ts
@@ -2,7 +2,7 @@ import core from 'smf-core';
 
 class Module1 {
   func1() {
-    core.log('module1, func1');
+    core.log('shared module1, func1');
   }
 }
 
--- core/shared/module2.ts
+++ core/shared/module2.ts
@@ -2,7 +2,7 @@ import core from 'smf-core';
 
 class Module2{
   func2() {
-    core.log('module1, func1');
+    core.log('shared module2, func2');
   }
 }
 

80a3e5a951e51d2a1f7ac359c2f54de173077685
Sergey Kravchenko <sergey@kazuhm.com>
2019-12-24 01:31:49 -0800

    Config format

--- bin/config.js
+++ bin/config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  PREFIX: 'smf-',
+  PREFIX:                        'smf-',
 
-  // STACK_BUILD_PATH: './build-stack',
-  STACK_BUILD_ENV_PATH: './build-stack/env',
-  STACK_BUILD_ENV_DEBUG_PATH: './build-stack/env/debug',
+  // STACK_BUILD_PATH:           './build-stack',
+  STACK_BUILD_ENV_PATH:          './build-stack/env',
+  STACK_BUILD_ENV_DEBUG_PATH:    './build-stack/env/debug',
 
-  STACK_CONFIG: 'smf-stack.json',
-  STACK_ENV: 'smf-env.json',
-  STACK_DOCKER_COMPOSE: 'smf-docker.yml',
+  STACK_CONFIG:                  'smf-stack.json',
+  STACK_ENV:                     'smf-env.json',
+  STACK_DOCKER_COMPOSE:          'smf-docker.yml',
   STACK_DOCKER_COMPOSE_SERVICES: 'smf-docker-services.yml',
-  STACK_DOCKER_NETWORK: 'main',
-  STACK_SERVICE_MANIFEST: 'smf-service.json',
-  STACK_SERVICE_NAME_SEPARATOR: '@',
+  STACK_DOCKER_NETWORK:          'main',
+  STACK_SERVICE_MANIFEST:        'smf-service.json',
+  STACK_SERVICE_NAME_SEPARATOR:  '@',
+  STACK_USAGE_EXAMPLE:           'usage-example.txt',
 
   session: {
     debug: false,

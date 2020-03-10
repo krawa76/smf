fa35599cc7dc8482ee469647ceb967d79229770c
Sergey Kravchenko <sergey@kazuhm.com>
2020-03-09 22:58:41 -0700

    Services config

---
+++ bin/config-services.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ALL: [
+    {'id': 'basic',            category: '', name: 'Basic',                 selectServices: true},
+    {'id': 'back-end-express', category: '', name: 'Back-end (Express.js)', selectServices: true},
+    // {'id': 'back-end-sails',   category: '', name: 'Back-end (Sails.js)', selectServices: true},
+    {'id': 'front-end-react',  category: '', name: 'Front-end (React.js)',  selectServices: false},
+  ]
+}

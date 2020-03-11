2f1eeecc47f36de9b97483cf7798f88117241879
Sergey Kravchenko <sergey@kazuhm.com>
2020-03-10 20:54:41 -0700

    Basic worker

--- bin/config-services.js
+++ bin/config-services.js
@@ -1,6 +1,6 @@
 module.exports = {
   ALL: [
-    {'id': 'basic',            category: '', name: 'Basic',                 selectClients: true},
+    {'id': 'basic',            category: '', name: 'Basic worker',          selectClients: true},
     {'id': 'back-end-express', category: '', name: 'Back-end (Express.js)', selectClients: true},
     // {'id': 'back-end-sails',   category: '', name: 'Back-end (Sails.js)', selectClients: true},
     {'id': 'front-end-react',  category: '', name: 'Front-end (React.js)',  selectClients: false},
